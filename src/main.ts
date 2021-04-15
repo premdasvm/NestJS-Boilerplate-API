14e7b9bc01926669fc1b44c6346f6b3d547d069a
Prem <premdasvm8@gmail.com>
2021-04-15 12:20:48 +0530

    Initial Commit, Swagger Configured

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors(CorsOptions);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle(`Parison's Salesforce API`)
+    .setDescription(
+      `Application Programming Interface For Parison's Salesforce application`,
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'JWT',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
