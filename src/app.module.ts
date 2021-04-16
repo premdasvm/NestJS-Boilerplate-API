2b6f737197964533b875804c0174ae2efe9bf0c0
Prem <premdasvm8@gmail.com>
2021-04-16 17:39:54 +0530

    [UPDATED] Modules Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigService } from './common/config/typeorm.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { GeneralModulesModule } from './modules/general-modules/general-modules.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    AuthModule,
+    UserModule,
+    GeneralModulesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
