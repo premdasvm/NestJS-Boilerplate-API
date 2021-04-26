5aa92a7551393893fc28bcd5c36732eea3ac1956
Prem <premdasvm8@gmail.com>
2021-04-26 21:54:03 +0530

    [UPDATED] Role Module Delete Route Override for Softdelete

--- src/modules/general-modules/roles/roles.service.ts
+++ src/modules/general-modules/roles/roles.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CrudRequest } from '@nestjsx/crud';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Role } from './entities/role.entity';
 
@@ -7,5 +8,10 @@ import { Role } from './entities/role.entity';
 export class RolesService extends TypeOrmCrudService<Role> {
   constructor(@InjectRepository(Role) repo) {
     super(repo);
+  }
+
+  async deleteOne(crudRequest: CrudRequest) {
+    const myEntity = await this.getOneOrFail(crudRequest);
+    return this.repo.softRemove(myEntity);
   }
 }
